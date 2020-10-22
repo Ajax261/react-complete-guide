3c12abe9d95110c60aac8c095b967b492e73ec2d
AjaxAm <amanrsahu@gmail.com>
2020-10-22 12:33:44 +0530

    AppJs: Added dynamic listing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./styles.css";
 import Person from "./Person/Person";
 
@@ -12,21 +12,27 @@ class App extends Component {
       { name: "Aman", age: "22" },
       { name: "Anisha", age: "23" }
     ],
-    showPersons: false
+    showPersons: true
   };
 
   render() {
     let persons = null;
 
     if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
+        </div>
+      );
     }
 
     return (
       <div className="App">
         <h1> Hey I am creating react App </h1>
         <button onClick={this.togglePersons}>Toggle Persons</button>
-        <Person name={this.state.persons[0].name} age="22" />
-        <Person name="Anisha" age="23" />
+        {persons}
       </div>
     );
   }
